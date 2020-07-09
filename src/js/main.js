a16a4d92aff9fc6a49230ee56515641b093b2bcc
Jeffrey Shen <jjshen@mit.edu>
2020-07-09 16:19:26 -0700

    handle resize

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,44 @@
-let margin = {top: 10, right: 20, bottom: 25, left: 50},
-  height = 250 - margin.top - margin.bottom,
-  width = 500 - margin.left - margin.right;
+function drawTimeSeries(data, xExtent, yExtent){
+  let margin = {top: 10, right: 20, bottom: 25, left: 50},
+    height = 250 - margin.top - margin.bottom,
+    width = document.getElementById("time-series").offsetWidth - margin.left - margin.right;
 
-function drawTimeSeries(data){
-  let svg = d3.select('#time-series')
+  console.log(width);
+
+  let svg = d3.select('#time-series').select("svg")
     .attr("width",  width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+    .select("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   let x = d3.scaleTime().range([0, width]),
       y = d3.scaleLinear().range([height, 0]);
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.y; }) + 20]);
+  x.domain(xExtent);
+  y.domain(yExtent);
 
   let valueline = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.y);  })
 
-  svg.append("path")
+  svg.select(".line")
     .data([data])
-    .attr("class", "line")
     .attr("d", valueline);
 
   // Add axes
-  let xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%Y")),
+  let xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%Y")).ticks(d3.timeMonth.every(6)),
       yAxis = d3.axisLeft(y);
 
-  svg.append("g").attr("transform", "translate(0," + height + ")").call(xAxis);
-  svg.append("g").call(yAxis);
+  svg.select(".xAxis").attr("transform", "translate(0," + height + ")").call(xAxis);
+  svg.select(".yAxis").call(yAxis);
 
   // Add axis labels
-  svg.append("text")
+  svg.select(".yAxisLabel")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left)
     .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .style("font-size", "12px")
-    .style("font-family", "IBM Plex Sans")
-    .text("Monthly Agency Contracts");
+    .attr("dy", "1em");
 }
-
 
 $.getJSON("http://127.0.0.1:3000/", function(data){ //TODO: fix this
   console.log(data);
@@ -63,8 +59,18 @@ $.getJSON("http://127.0.0.1:3000/", function(data){ //TODO: fix this
     .map(function(d){return {date: new Date(d), y:  newAgencyLine[d]}})
     .sort(function(a, b){return a.date - b.date});
 
-  console.log(newAgencyLine);
+  let xExtent = d3.extent(newAgencyLine, function(d) { return d.date; }),
+      yExtent = [0, d3.max(newAgencyLine, function(d) { return d.y; }) + 20];
 
   // Draw Graphs
-  drawTimeSeries(newAgencyLine);
+  drawTimeSeries(newAgencyLine, xExtent, yExtent);
+
+  // Add resize handlers
+  let resizeEnd;
+  $(window).resize(function () {
+    clearTimeout(resizeEnd);
+    resizeEnd = setTimeout(function() {
+      drawTimeSeries(newAgencyLine, xExtent, yExtent);
+    }, 100);
+  });
 });
